910d66a647aea541a4c4c51bea610c47ba9c3a2d
Hugo Tamaki <hugokt2005@yahoo.com.br>
2017-02-25 15:22:05 -0300

    Adding namespace to admin endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, as: nil, defaults: {format: :json} do
+    namespace :v1, as: nil do
+      namespace :admin, as: nil do
+
+      end
+    end
+  end
 end
