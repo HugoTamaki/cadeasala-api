a22309d0272521657d6b68000eb05ba13f4d8bbf
Hugo Tamaki <hugokt2005@yahoo.com.br>
2017-03-11 06:37:42 -0300

    debug

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,4 @@
 class Users::SessionsController < Devise::SessionsController
-  include Devise::Controllers::Helpers
-  
   skip_before_action :validate_token, only: :create
   skip_after_action :update_token, only: :create
   # respond_to :json
@@ -11,7 +9,9 @@ class Users::SessionsController < Devise::SessionsController
   def create
     resource = User.find_by(email: user_params[:email])
     if resource && resource.valid_password?(user_params[:password])
+      puts resource
       @authenticated_user = Api::V1::Admin::AdminJWTSerializer.new(resource).serializable_hash
+      puts @authenticated_user
       response.headers['XSRF-TOKEN'] = Login.generate_token(@authenticated_user)
       render json: resource, serializer: Api::V1::UserSerializer
     else
