0443f71d49697284062c53d9008188ad249346c3
Hugo Tamaki <hugokt2005@yahoo.com.br>
2017-03-11 07:04:46 -0300

    Using secret token instead

--- app/models/login.rb
+++ app/models/login.rb
@@ -2,14 +2,14 @@ class Login
   class << self
     def validate_token(token)
       begin
-        JWT.decode token, Rails.application.secrets.secret_key_base, true, { algorithm: 'HS256' }
+        JWT.decode token, Rails.application.secrets.secret_token, true, { algorithm: 'HS256' }
       rescue JWT::ImmatureSignature, JWT::ExpiredSignature, JWT::DecodeError
         nil
       end
     end
 
     def generate_token(data)
-      JWT.encode data, Rails.application.secrets.secret_key_base, 'HS256'
+      JWT.encode data, Rails.application.secrets.secret_token, 'HS256'
     end
   end
 end
