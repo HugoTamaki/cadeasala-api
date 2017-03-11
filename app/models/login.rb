b361e33d6d7ea7abb31b2097f134ff0f7e47c889
Hugo Tamaki <hugokt2005@yahoo.com.br>
2017-03-11 06:58:36 -0300

    Setting secret base key from secrets yml

--- app/models/login.rb
+++ app/models/login.rb
@@ -2,14 +2,14 @@ class Login
   class << self
     def validate_token(token)
       begin
-        JWT.decode token, ENV['SECRET_BASE_KEY'], true, { algorithm: 'HS256' }
+        JWT.decode token, Rails.application.secrets.secret_key_base, true, { algorithm: 'HS256' }
       rescue JWT::ImmatureSignature, JWT::ExpiredSignature, JWT::DecodeError
         nil
       end
     end
 
     def generate_token(data)
-      JWT.encode data, ENV['SECRET_BASE_KEY'], 'HS256'
+      JWT.encode data, Rails.application.secrets.secret_key_base, 'HS256'
     end
   end
 end
